0e499b828a9a4e890f2240cdd0bd3a001c20a57e
Yassine Baadi <baadiyassine51@gmail.com>
2025-12-09 20:26:36 +0100

    Update problem_29.c

--- algorithms/level_01/problem_29.c
+++ algorithms/level_01/problem_29.c
@@ -2,7 +2,7 @@
 
 int main ()
 {
-        float numHours, numDays, numWeeks;
+    float numHours, numDays, numWeeks;
 	int day, week;
 	day = 24;
 	week = 7;
@@ -10,7 +10,7 @@ int main ()
         printf("Enter number of hours: ");
         scanf("%f", &numHours);
 
-        numDays = numHours / day;
+    numDays = numHours / day;
 	numWeeks = numDays / week;
 
         printf("%.2f Weeks\n%.2f Days\n", numWeeks, numDays);
